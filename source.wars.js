8224feb117e483bb303a597c32ff38302fbf082a
WarsFeng <huangrongfeng1999@gmail.com>
2017-09-04 14:07:07 +0800

    add domain name

--- source.wars.js
+++ source.wars.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         闲鱼搜索框显示
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.6
 // @description  try to take over the world!
 // @author       Wars
 // @match        https://2.taobao.com/*

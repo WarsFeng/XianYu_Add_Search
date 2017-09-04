6a01ed40f8d7c0d16826193257292142e1c676fa
WarsFeng <huangrongfeng1999@gmail.com>
2017-09-04 14:35:02 +0800

    Update min_source

--- source.wars.js
+++ source.wars.js
@@ -15,7 +15,7 @@
     //搜索框恢复
     var Header_Search = document.createElement("div");
     Header_Search.className="idle-search";
-    Header_Search.innerHTML="<form method=\"get\" action=\"//s.2.taobao.com/list/list.htm\" name=\"search\" target=\"_top\"><input class=\"input-search\" id=\"J_HeaderSearchQuery\" name=\"q\" type=\"text\" value=\"\" placeholder=\"搜闲鱼\" /><input type=\"hidden\" name=\"search_type\" value=\"item\" autocomplete=\"off\" /><input type=\"hidden\" name=\"app\" value=\"shopsearch\" autocomplete=\"off\" /><button class=\"btn-search\" type=\"submit\"><i class=\"iconfont\">&#xe602;</i><span class=\"search-img\"></span></button></form>";
+    Header_Search.innerHTML='<form method="get" action="//s.2.taobao.com/list/list.htm" name="search" target="_top"><input class="input-search" id="J_HeaderSearchQuery" name="q" type="text" value="" placeholder="搜闲鱼" /><input type="hidden" name="search_type" value="item" autocomplete="off" /><input type="hidden" name="app" value="shopsearch" autocomplete="off" /><button class="btn-search" type="submit"><i class="iconfont">&#xe602;</i><span class="search-img"></span></button></form>';
     document.getElementById("J_IdleHeader").appendChild(Header_Search);
     //去除商品第一张图片广告
     var guide_img = document.getElementsByClassName("guide-img");

2cff933b2a59e95ed51677ec3d6ea7d19d3fd6e3
WarsFeng <huangrongfeng1999@gmail.com>
2017-09-04 14:40:49 +0800

    Last Update

--- source-min.js
+++ source-min.js
@@ -1,1 +1,12 @@
+// ==UserScript==
+// @name         闲鱼搜索框显示
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       Wars
+// @match        https://2.taobao.com/*
+// @match        https://s.2.taobao.com/*
+// @match        https://trade.2.taobao.com/*
+// @grant        none
+// ==/UserScript==
 (function(){var a=document.createElement("div");a.className="idle-search";a.innerHTML='<form method="get" action="//s.2.taobao.com/list/list.htm" name="search" target="_top"><input class="input-search" id="J_HeaderSearchQuery" name="q" type="text" value="" placeholder="搜闲鱼" /><input type="hidden" name="search_type" value="item" autocomplete="off" /><input type="hidden" name="app" value="shopsearch" autocomplete="off" /><button class="btn-search" type="submit"><i class="iconfont">&#xe602;</i><span class="search-img"></span></button></form>';document.getElementById("J_IdleHeader").appendChild(a);var b=document.getElementsByClassName("guide-img");b[0].parentNode.innerHTML=""})();
--- source.wars.js
+++ source.wars.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         闲鱼搜索框显示
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      1.0
 // @description  try to take over the world!
 // @author       Wars
 // @match        https://2.taobao.com/*
